ee49599f41f99cb8bfa91c7115e2b9137935d171
05sanjaykumar <0508sanjaykumar@gmail.com>
2026-02-19 21:22:48 +0530

    chore: Some bugs in code on calling number

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express, {  Request, Response } from "express";
 import dotenv from "dotenv";
 
+import twilio from "twilio";
 dotenv.config();
+
+const client = twilio(
+  process.env.TWILIO_SID!,
+  process.env.TWILIO_AUTH!
+);
+
 
 const app = express();
 
@@ -11,6 +18,8 @@ app.use(express.json());
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Server is alive");
+  console.log("SID:", process.env.TWILIO_SID);
+  console.log("AUTH:", process.env.TWILIO_AUTH);
 });
 
 app.post("/call-status", (req: Request, res: Response) => {
@@ -25,6 +34,21 @@ app.post("/call-status", (req: Request, res: Response) => {
   }
 
   res.sendStatus(200);
+});
+
+app.get("/test-call", async (_req: Request, res: Response) => {
+  try {
+    await client.calls.create({
+      to: process.env.MY_PHONE!,
+      from: process.env.TWILIO_NUMBER!,
+      url: "http://demo.twilio.com/docs/voice.xml"
+    });
+
+    res.send("Calling your phone...");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Call failed");
+  }
 });
 
 app.listen(3000, () => {
